3becb303ffe391712c48aa215c1f9cc6ab3f7593
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-02-14 23:51:33 +0530

    React-project

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>app</h1>
+      <img src="" alt="" />
+      <div>
+        hapy
+      </div>
+    </div>
+  )
+}
+
+export default App
+ 
