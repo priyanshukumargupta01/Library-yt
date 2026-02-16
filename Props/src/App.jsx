b98c1fb6075f0ca84f25a36d01d27b83c9114913
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-02-16 22:46:17 +0530

    html  css

---
+++ Props/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="card">
+        <h1>PriyanshuKumar</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
