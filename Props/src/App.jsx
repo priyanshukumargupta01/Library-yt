f482c35f10878aa7211174fa430f46afe12fbbcc
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-02-17 23:24:32 +0530

    moer

--- Props/src/App.jsx
+++ Props/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import profile from './assets/profile.jpg'
 
 const App = () => {
   return (
     <div>
       <div className="card">
+        <img src={profile} alt="" />
         <h1>PriyanshuKumar</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut minus alias nobis.</p>
+        <button>Click Me</button>
       </div>
     </div>
   )
