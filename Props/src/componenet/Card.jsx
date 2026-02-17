056f4a4794c0e21ede1bb9e899645d2cf68300a7
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-02-17 23:38:42 +0530

    more

---
+++ Props/src/componenet/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react' 
+
+const Card = () => {
+  return (
+     
+      <div className="card">
+              <img src="https://th.bing.com/th/id/OIP.khwgAYyArCkpLh6lglOJ1gAAAA?w=236&h=420&c=7&o=5&cb=defcache2&dpr=1.3&pid=1.20&defcache=1" alt="" />
+              <h1>PriyanshuKumar</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur  8adipisicing elit. Ut minus alias nobis.</p>
+              <button>Click Me</button>
+            </div>
+   )
+}
+
+export default Card
