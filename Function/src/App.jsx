c4fcf3249787d3f8da7ac76c159787aea9359f16
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-02-21 01:10:10 +0530

    more

---
+++ Function/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const App = () => {
+
+//   const hi= document.getElementsByClassName(".bbb")
+// const btnClicked =()=>{
+//    hi.addEventListener("click",()=>{
+//     hi.innerHtml="hu"
+ 
+//    btnClicked();
+
+//   })
+// }
+function btnClicked(){
+  document.querySelector()
+}
+ return (
+    <div>
+      <h1>HI, Priyanshu</h1>
+      <button onClick={btnClicked}>Change User</button>
+    </div>
+  );
+};
+
+export default App;
