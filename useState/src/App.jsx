8860010b8bbebee05372842a301c61a8ee706857
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-02-22 22:29:33 +0530

    more

--- useState/src/App.jsx
+++ useState/src/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  // let a = 20;
 
-export default App
+  // function changeA() {
+  //   console.log(a);
+  //   a = 40;
+  //   console.log(a);
+  // }
+
+
+  const [num, setNumm] = useState(90)
+  const [name, setName] = useState("Priyanshu")
+
+function changeA() { 
+    setNumm(70)
+    setName("priyanshu kumar")
+  }
+// function changeB() { 
+//     setName("priyanshu kumar");
+//   }
+  
+  return (
+    <div>
+      <h1>vlaue of a is {num}</h1>
+      <h1>my name is {name}</h1>
+      <button onClick={changeA} >click for change</button>
+    </div>
+  );
+};
+
+export default App;
