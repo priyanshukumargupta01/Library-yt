ec7cb3665322127e924b387f94955e4df44a4023
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-03-08 23:21:37 +0530

    More

--- API-Calling/src/App.jsx
+++ API-Calling/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
 const App = () => {
+
+  const btnClicked =()=>{
+   console.log("iiiii");
+   
+  }
+
   return (
-    <div>App</div>
+    <div>
+      <button onClick={btnClicked}>Click</button>
+    </div>
   )
 }
 
