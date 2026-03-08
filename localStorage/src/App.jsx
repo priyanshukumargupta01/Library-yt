de17e8c364d9ab36eff504e16073e50741fe481c
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-03-08 22:53:54 +0530

    More

--- localStorage/src/App.jsx
+++ localStorage/src/App.jsx
@@ -1,6 +1,28 @@
 import React from 'react'
 
 const App = () => {
+
+  // sessionStorage.clear()
+  // localStorage.clear()
+
+  /*
+  // localStorage.setItem("user","Priyanshu")
+  // localStorage.setItem("Age","20") 
+  const user = localStorage.getItem("user")
+  const age = localStorage.getItem("Age")
+  console.log(user,age);
+  */
+
+  // const user={
+  //  name: "pk",
+  //  age:20,
+  //  city:"gaya"
+  // }
+  // localStorage.setItem("user",JSON.stringify(user))
+  const a = JSON.parse(localStorage.getItem("user"))
+  console.log(a);
+  
+
   return (
     <div>App</div>
   )
