cb2a85799bf8a1aac677e1e1cc2dfbdbb3f4fb5d
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-03-18 00:13:26 +0530

    More

--- useEffect/src/App.jsx
+++ useEffect/src/App.jsx
@@ -3,17 +3,23 @@ import React, { useEffect, useState } from 'react'
 const App = () => {
 
   const [num, setNum] = useState(0)
+  const [num2, setNum2] = useState(10)
 
   useEffect(() => {
     console.log("hi")
-  }, []) 
+  }, [num]) 
 
   return (
     <div>
-      <h1>{num}</h1>
-      <button onClick={() => {
-        setNum(num + 1)
-      }}>
+      <h1>num  1 is : {num}</h1>
+      <h1>num  2 is : {num2}</h1>
+      <button 
+      onMouseEnter={() => {
+        setNum(num + 1) 
+      }}
+      onMouseLeave={()=>{
+        setNum2(num2 + 1)
+      }   }>
         click
       </button>
     </div>
