b5ebeaba972d401d9051d5c13dddc2807a4749da
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-03-20 17:37:25 +0530

    MORE

--- Router/src/App.jsx
+++ Router/src/App.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Navbar from './Component/Navbar';
 
 const App = () => {
   return (
     <div>
+     <Navbar />
       <Routes>
         <Route path='/' element={<Home />}/> 
-        <Route path='/' element={<About />}/> 
+        <Route path='/about' element={<About />}/> 
+        <Route path='/contact' element={<Contact />}/>
       </Routes> 
-<h1>yujhvm</h1>
     </div>
   )
 }
+
 
 export default App
---
+++ Router/src/Component/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+     <div>
+          <a href="Priyanshu"></a>
+          <div>
+            <a href="Home"></a>
+            <a href="About"></a>
+            <a href="Contact"></a>
+          </div>
+    </div>
+  )
+}
+
+export default Navbar
