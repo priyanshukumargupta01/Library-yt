44cb84c0e273e46b3ff3e67dbb2720036be0c245
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-03-20 10:40:21 +0530

    MORE

--- Router/src/App.jsx
+++ Router/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />}/> 
+        <Route path='/' element={<About />}/> 
+      </Routes> 
+<h1>yujhvm</h1>
+    </div>
   )
 }
 
